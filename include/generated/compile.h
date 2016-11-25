0cc32cf88b58d8cb47e704366b57e90839a7057b
Hoa Nguyen <hoalamha@gmail.com>
2016-11-25 09:50:43 +0700

    init from samsung open source

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm64"
+#define UTS_VERSION "#1 SMP PREEMPT Fri Nov 25 09:41:06 ICT 2016"
+#define LINUX_COMPILE_BY "hoalamha"
+#define LINUX_COMPILE_HOST "tekcafe"
+#define LINUX_COMPILER "gcc version 4.9.x 20150123 (prerelease) (GCC) "
